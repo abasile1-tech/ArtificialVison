7df958a6133ca035a5ec7d3265faa5f1b25823b7
John Basile <ashton.basile@gmail.com>
2026-09-02 19:47:27 +0100

    Add project files.

---
+++ ArtificialVison/ArtificialVison.h
@@ -0,0 +1,7 @@
+﻿// ArtificialVison.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+// The first simulator intentionally has no third-party dependencies. It can be
+// used as a stable base before a graphical renderer or OpenCV is introduced.
